0858fd82557cded6093ea9b3456f798c4f09f004
Florian Klaessen <42743938+Klaessen@users.noreply.github.com>
2022-05-30 12:28:44 +0200

    Update interceptors.js

--- interceptors.js
+++ interceptors.js
@@ -1,14 +1,14 @@
-//fetch api interceptor
+//fetch interceptor
 const origFetch = window.fetch
 fetch = async (...args) => {
 
     let fetchUrl = ''
     if ('url' in args[0]){
         //Object.defineProperty(args[0],'url', {writable: true, enumerable: true, configurable: true})
-        fetchUrl = 'http://localhost:8000/content/'+btoa(args[0]['url']+'?'+args[0]['method']);
+        fetchUrl = 'http://test.example.com/content/'+btoa(args[0]['url']+'?'+args[0]['method']);
 
     } else {
-        fetchUrl = 'http://localhost:8000/anonymize/'+btoa(args[0]);
+        fetchUrl = 'http://test.example.com/anonymize/'+btoa(args[0]);
     }
 
     //create new request since request object is immutable
@@ -20,28 +20,34 @@ fetch = async (...args) => {
     //intercept result if needed
     var result = await origFetch(request)
 
-    //set a tags
+/*    //set a tags
     $("a").each(function() {
         if (this.href !== 'http://127.0.0.1:8000/' && !this.href.startsWith('http://')) {
             $(this).attr("href", 'http://localhost:8000/anonymize/'+(btoa(this.href)));
         }
-    });
+    });*/
 
     return result;
 }
 
 //ajax interceptor
-!function(){
-    XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
+let oldXHROpen = window.XMLHttpRequest.prototype.open;
+window.XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
 
-        url = 'http://localhost:8000/content/'+btoa(url);
-
-        open.apply(this, arguments);
+    var requestUrl = arguments[1].startsWith('http') ? arguments[1] :  $("base").attr("href").val()+arguments[1];
+    let requestObject = {
+        0: arguments[0],
+        1: 'http://test.example.com/content/'+btoa(requestUrl+'?'+arguments[0]),
+        2: arguments[2],
+        3: arguments[3] ?? null,
+        4: arguments[4] ?? null,
     };
-    //set a tags
-    $("a").each(function() {
-        if (this.href !== 'http://127.0.0.1:8000/' && !this.href.startsWith('http://')) {
-            $(this).attr("href", 'http://localhost:8000/content/'+(btoa(this.href)));
-        }
+
+    console.log(requestObject);
+    this.addEventListener('load', function() {
+        console.log('load: ' + this.responseText);
+        // Hide loader
     });
-}();
+
+    return oldXHROpen.apply(this, requestObject);
+}
